ea53aa796a32067c2fe2cab73b58576044940b98
Smit Shah <who828@gmail.com>
2013-02-17 23:16:39 +0530

    Added ex1.11 and ex1.12 solutions

---
+++ ch1/ex1.12.rkt
@@ -0,0 +1,5 @@
+(define (pascal row col)
+   (cond ((< row col) #f)
+         ((or (= 0 col) (= row col)) 1)
+         (else (+ (pascal (- row 1) col)
+                  (pascal (- row 1) (- col 1))))))
