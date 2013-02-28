4def060633863a5f84912e710c763b2f1f79c7a9
Smit Shah <who828@gmail.com>
2013-02-28 10:06:30 +0530

    Added ex2.59-2.62 solutions

---
+++ ch2/ex2.59.rkt
@@ -0,0 +1,11 @@
+(define (union-set set1 set2)
+  (cond ((null? set1) set2)
+        ((null? set2) set1)
+        ((element-of-set? (car set1) set2)
+         (union-set (cdr set1) set2))
+        (else (cons (car set1) (union-set (cdr set1) set2)))))
+
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((equal? x (car set)) true)
+        (else (element-of-set? x (cdr set)))))
